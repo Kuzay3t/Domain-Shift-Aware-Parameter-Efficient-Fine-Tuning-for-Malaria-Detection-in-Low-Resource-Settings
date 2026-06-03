7e4fc825fe00aef866d5620e429d5a20d83d92b0
Glory Bagai <kuzayet.m2204354@st.futminna.edu.ng>
2026-06-03 15:33:06 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ds_aware_lora",
+    version="0.1.0",
+    description="Domain-shift-aware LoRA for medical imaging",
+    author="Your Name",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[...],
+)
